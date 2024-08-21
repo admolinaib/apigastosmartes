da057437813ae6999fd9fdbdd10c9c59b64e4376
diseño y desarrollo <disenoydesarrollo@academicos.cesde.edu.co>
2024-08-20 20:25:41 -0500

    nt13 se descargan dependencias y se crean estructura de carpetas feat

---
+++ app/database/configuration.py
@@ -0,0 +1,8 @@
+'''userDB = "usuario"
+userAge = 35
+userHeight = 1.62
+userIsHappy = True
+userPassword = None
+
+userCity = input("En que ciudad Nacio: ")
+'''
---
+++ main.py
@@ -0,0 +1,1 @@
+#archivo pricipal del api
